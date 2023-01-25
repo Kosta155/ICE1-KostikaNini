328e5ac2ffc433d1bd838a5d30c902eeba42d074
Kosta155 <122299818+Kosta155@users.noreply.github.com>
2023-01-24 19:38:19 -0500

    Update Card.java v2

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -67,18 +67,18 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Kostika, but you can call me Kosta");
         System.out.println();
         
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
+        System.out.println("-- Be a good programmer and web developer!");
         System.out.println("-- Have a semester with no violations of academic integrity!");
 	System.out.println();	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
+        System.out.println("-- Soccer");
+        System.out.println("-- Chess");
+        System.out.println("-- Gym");
         System.out.println("-- Riding my motorcycle");
 
         System.out.println();
